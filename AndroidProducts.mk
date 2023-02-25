c5cea1d464dc4bebca25e8534716f005ef4a137a
Sumenus <54213145+Sumenus@users.noreply.github.com>
2023-02-25 17:11:47 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/syberia_sweet.mk
     
 COMMON_LUNCH_CHOICES := \
-    syberia_oneplus6-user \
-    syberia_oneplus6-userdebug \
-    syberia_oneplus6-eng
+    syberia_sweet-user \
+    syberia_sweet-userdebug \
+    syberia_sweet-eng
