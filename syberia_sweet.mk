d9fc71847b9716c32e7cd265678a4e6130057ad3
Sumenus <54213145+Sumenus@users.noreply.github.com>
2023-02-25 15:59:18 +0300

    Update syberia_sweet.mk

--- syberia_sweet.mk
+++ syberia_sweet.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
 
 PRODUCT_NAME := syberia_sweet
 PRODUCT_DEVICE := sweet
